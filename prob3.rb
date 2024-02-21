49739c942065f3913cac284d770ac13d6c0afb82
kieloh <nathan563412@gmail.com>
2024-02-20 20:36:12 -0600

    finished problem 3

---
+++ prob3.rb
@@ -0,0 +1,15 @@
+class User
+  attr_reader :username
+  def initialize(username)
+    @username = username
+  end
+
+  def username=(username)
+    raise ArgumentError, "Name cannot be empty" if username.to_s.strip.empty?
+      @username = username
+  end
+end
+
+user = User.new("kieloh")
+puts user.username
+user.username = ""
